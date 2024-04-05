4c52997f2460f0aa48000971a5d9df861c4db01a
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-04-05 10:52:56 -0500

    conf: support env validation at run time with jiti

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,12 @@
+import createJITI from "jiti";
+import { fileURLToPath } from "node:url";
+
+// import env vairables here to validate during build
+const jiti = createJITI(fileURLToPath(import.meta.url));
+
+jiti("./src/config/env/client.ts");
+jiti("./src/config/env/server.ts");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
